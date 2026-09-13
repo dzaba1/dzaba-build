45aab7a51738447b3bf0120bc37580a0a1e548e6
dzaba1 <blazej.zabialowicz@gmail.com>
2026-09-13 14:31:56 +0200

    More libs

---
+++ examples/monorepo/Lib10/AssemblyPrinter.cs
@@ -0,0 +1,17 @@
+using FrameworkGetter;
+using Lib1;
+
+namespace Lib10;
+
+public static class AssemblyPrinter
+{
+    public static string GetFullAssemblyString(int indent)
+    {
+        var str = Printer.GetFullAssemblyString(indent, typeof(AssemblyPrinter), Framework.GetCurrentFramework(),
+            Lib1.AssemblyPrinter.GetFullAssemblyString,
+            Lib2.AssemblyPrinter.GetFullAssemblyString,
+            Lib3.AssemblyPrinter.GetFullAssemblyString,
+            Lib5.AssemblyPrinter.GetFullAssemblyString);
+        return $"{new string(' ', indent)}{str}";
+    }
+}
---
+++ examples/monorepo/Lib11/AssemblyPrinter.cs
@@ -0,0 +1,16 @@
+using FrameworkGetter;
+using Lib1;
+
+namespace Lib11;
+
+public static class AssemblyPrinter
+{
+    public static string GetFullAssemblyString(int indent)
+    {
+        var str = Printer.GetFullAssemblyString(indent, typeof(AssemblyPrinter), Framework.GetCurrentFramework(),
+            Lib1.AssemblyPrinter.GetFullAssemblyString,
+            Lib4.AssemblyPrinter.GetFullAssemblyString,
+            Lib5.AssemblyPrinter.GetFullAssemblyString);
+        return $"{new string(' ', indent)}{str}";
+    }
+}

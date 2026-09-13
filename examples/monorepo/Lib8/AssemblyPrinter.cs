b9fea3c8b773b0f03638eba3a46b3db94f4b475c
dzaba1 <blazej.zabialowicz@gmail.com>
2026-09-13 14:21:40 +0200

    More libs

---
+++ examples/monorepo/Lib8/AssemblyPrinter.cs
@@ -0,0 +1,14 @@
+using FrameworkGetter;
+using Lib1;
+
+namespace Lib8;
+
+public static class AssemblyPrinter
+{
+    public static string GetFullAssemblyString(int indent)
+    {
+        var str = Printer.GetFullAssemblyString(indent, typeof(AssemblyPrinter), Framework.GetCurrentFramework(),
+            i => Lib1.AssemblyPrinter.GetFullAssemblyString(i));
+        return $"{new string(' ', indent)}{str}";
+    }
+}

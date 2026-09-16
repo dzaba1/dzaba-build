23d4c1879dd0e3fa92379c6225af96e036d9991d
dzaba1 <blazej.zabialowicz@gmail.com>
2026-09-16 12:07:19 +0200

    Optimization

--- examples/monorepo/Lib11/AssemblyPrinter.cs
+++ examples/monorepo/Lib11/AssemblyPrinter.cs
@@ -8,7 +8,6 @@ public static class AssemblyPrinter
     public static string GetFullAssemblyString(int indent)
     {
         var str = Printer.GetFullAssemblyString(indent, typeof(AssemblyPrinter), Framework.GetCurrentFramework(),
-            Lib1.AssemblyPrinter.GetFullAssemblyString,
             Lib4.AssemblyPrinter.GetFullAssemblyString,
             Lib5.AssemblyPrinter.GetFullAssemblyString);
         return $"{new string(' ', indent)}{str}";
